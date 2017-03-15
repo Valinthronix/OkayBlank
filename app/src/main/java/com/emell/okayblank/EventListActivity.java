6f60e54346c9081f46e36133fa40ad111f910802
Valinthronix <jmeyerlee@gmail.com>
2017-03-15 18:57:45 -0400

    Ayyy the nav drawer works

--- app/src/main/java/com/emell/okayblank/EventListActivity.java
+++ app/src/main/java/com/emell/okayblank/EventListActivity.java
@@ -6,7 +6,7 @@ import android.support.v4.app.Fragment;
  * Created by Val on 2/15/2017.
  */
 
-public class EventListActivity extends SingleFragmentActivity {
+public class EventListActivity extends DrawerWithFragmentActivity {
 
 	@Override
 	protected Fragment createFragment(){
